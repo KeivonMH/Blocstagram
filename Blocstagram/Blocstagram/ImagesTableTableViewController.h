9680265848a899c81267abdbc33559d42b850528
Keivon Hobeheidar <KeivonMH@gmail.com>
2015-09-06 18:00:42 -0700

    Blocstagram Feed

---
+++ Blocstagram/Blocstagram/ImagesTableTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableTableViewController.h
+//  Blocstagram
+//
+//  Created by Keivon Hobeheidar on 9/6/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
